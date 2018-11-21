b46a0d91b13297f02f6e1341a010fd7f79c590ab
Nora Vogt <noraevogt@gmail.com>
2018-11-21 16:34:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(collection)
   new_array = []
 
   while i < collection.length
-    yield(collection[i])
-    new_array << collection[i]
+    new_array << yield(collection[i])
+
     i += 1
   end
   new_array
